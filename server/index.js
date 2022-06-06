0e1ab98e5a3c6e28c8028f21c0d2ee4977102243
pq43 <n.43.mehta@gmail.com>
2022-06-05 22:35:50 -0700

    docker compose files

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.json({ message: "running" });
+  res.json({ message: "Example Response test again Lol" });
 });
 
 app.listen(port, () => {
   console.log(
-    `${new Date().toLocaleTimeString()} | [INFO] Listening to requests on port ${port}`
+    `${new Date().toLocaleTimeString()} | [INFO] Listening to requests on port ${port}.`
   );
 });
